68297f457c932df28bdaa427e9d9b1bbf7ac50a6
Satabdi <satabdi.d@gmail.com>
2018-10-21 16:20:55 -0400

    Adding MorganAndString Updates
    Adding MorganAndString Updates

--- src/org/practice/MorganAndString.java
+++ src/org/practice/MorganAndString.java
@@ -17,19 +17,33 @@ public class MorganAndString {
 	static String morganAndString(String a, String b) {
 		String result = "";
 		
-		List<String> listA = new ArrayList<String>(Arrays.asList(a));
-		List<String> listB = new ArrayList<String>(Arrays.asList(b));
+		char[] charA = a.toCharArray();
+		List<Character> listA = new ArrayList<>();
+		for (char c : charA) {
+			//System.out.print(c+" ");
+			listA.add(c);
+		}
+		
+		char[] charB = b.toCharArray();
+		List<Character> listB = new ArrayList<>();
+		for (char c : charB) {
+			//System.out.print(c+" ");
+			listB.add(c);
+		}		
+		
 		List res = new ArrayList<>();
 		
-		System.out.println("ListA :" +listA);
-		System.out.println("ListB :" +listB);
+		System.out.println(listA);
+		System.out.println(listB);
 		
-		System.out.println(listA.get(0).compareTo(listB.get(0)));
-		System.out.println(listB.get(0).compareTo(listA.get(0)));
+		System.out.println(listA.get(0)>listB.get(0));
+		System.out.println(listA.get(0)<listB.get(0));
+		
+		
 		
 		
 		while (listA.size() > 0 && listB.size()>0) {
-			if(listA.get(0).compareTo(listB.get(0)) > 0){
+			if(listA.get(0) < listB.get(0)){
 				res.add(listA.get(0));
 				listA.remove(0);
 			}
@@ -39,9 +53,19 @@ public class MorganAndString {
 			}
 		}
 		
+		if(!listA.isEmpty()){
+			for (char c : listA) {
+				res.add(c);
+			}	
+		} else{
+			for (char c : listB) {
+				res.add(c);
+			}	
+		}
+		
 		System.out.println(res);
 		
-		return result;
+		return res.toString();
     }
 
 }
